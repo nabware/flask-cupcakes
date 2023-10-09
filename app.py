0e9ee955adec1ef03531fe39ee8443ff64ac6283
Nabeel Rana <nabwaregroup@gmail.com>
2023-10-09 11:34:11 -0700

    Completed part 1

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
 """Flask app for Cupcakes"""
+
+import os
+
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", 'postgresql:///cupcakes')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
+
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
--- models.py
+++ models.py
@@ -1,1 +1,49 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = "https://tinyurl.com/demo-cupcake"
+
+class Cupcake(db.Model):
+    """Cupcake"""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    flavor = db.Column(
+        db.String(50),
+        db.CheckConstraint("LENGTH(flavor) >= 2"),
+        nullable=False
+    )
+
+    size = db.Column(
+        db.String(15),
+        db.CheckConstraint("LENGTH(size) >= 2"),
+        nullable=False
+    )
+
+    rating = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    image_url = db.Column(
+        db.String(500),
+        db.CheckConstraint("LENGTH(image_url) >= 14"),
+        nullable=False,
+        default=DEFAULT_IMAGE_URL
+    )
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
